2c3b45c7353e10611793a3977f8f42c487d5c6c4
Simon Harvey <simon.harvey.86@gmail.com>
2023-12-11 12:02:26 -0500

    Create run_before_01-install-zsh.sh

---
+++ run_before_01-install-zsh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+if ! command -v zsh >/dev/null; then
+  sudo apt update && sudo apt install zsh -y
+  echo "ZSH package installed."
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
+  echo "oh-my-zsh package installed."
+fi
+
+if ! command -v git >/dev/null; then
+  sudo apt update
+  sudo apt install git -y
+  echo "Git package was installed, required for the next step."
+fi
