93a60649ceea21b78ad5d8723e60d1f1194e7951
Simon Harvey <simon.harvey.86@gmail.com>
2023-12-11 12:05:06 -0500

    Update run_before_03-install-ansible.sh

--- run_before_03-install-ansible.sh
+++ run_before_03-install-ansible.sh
@@ -6,3 +6,6 @@ sudo apt update && sudo apt install pipx -y
 pipx install --include-deps ansible
 pipx ensurepath
 echo "Ansible package installed."
+
+ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
+echo "Ansible installation complete."
